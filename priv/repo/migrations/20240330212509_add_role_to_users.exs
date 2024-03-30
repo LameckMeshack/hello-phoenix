0b76e6fdb1d5156cd491a4f6288b11072425cbfa
LameckMeshack <otienolameck327@gmail.com>
2024-03-31 00:27:50 +0300

    Add role column to users table

---
+++ priv/repo/migrations/20240330212509_add_role_to_users.exs
@@ -0,0 +1,9 @@
+defmodule Hello.Repo.Migrations.AddRoleToUsers do
+  use Ecto.Migration
+
+  def change do
+    alter table(:users) do
+      add :role, :string, default: "user"
+    end
+  end
+end
