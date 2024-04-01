ef0b3e0a6eedb3556d5f016802211409c67a7a17
LameckMeshack <otienolameck327@gmail.com>
2024-04-01 23:48:16 +0300

    Update cart_items table schema

--- priv/repo/migrations/20240401202621_create_cart_items.exs
+++ priv/repo/migrations/20240401202621_create_cart_items.exs
@@ -3,15 +3,15 @@ defmodule Hello.Repo.Migrations.CreateCartItems do
 
   def change do
     create table(:cart_items) do
-      add :price_when_carted, :decimal
+      add :price_when_carted, :decimal, precision: 15, scale: 6, null: false
       add :quantity, :integer
-      add :cart_id, references(:carts, on_delete: :nothing)
-      add :product_id, references(:products, on_delete: :nothing)
+      add :cart_id, references(:carts, on_delete: :delete_all)
+      add :product_id, references(:products, on_delete: :delete_all)
 
       timestamps(type: :utc_datetime)
     end
 
-    create index(:cart_items, [:cart_id])
     create index(:cart_items, [:product_id])
+    create unique_index(:cart_items, [:cart_id, :product_id])
   end
 end
