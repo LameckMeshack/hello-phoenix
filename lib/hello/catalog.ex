a5af2372a7df2651bdafc9abd83a97c8a1900724
LameckMeshack <otienolameck327@gmail.com>
2024-04-01 17:31:01 +0300

    Add page view increment functionality to product show endpoint

--- lib/hello/catalog.ex
+++ lib/hello/catalog.ex
@@ -101,4 +101,12 @@ defmodule Hello.Catalog do
   def change_product(%Product{} = product, attrs \\ %{}) do
     Product.changeset(product, attrs)
   end
+
+  def inc_page_views(%Product{} = product) do
+    {1, [%Product{views: views}]} =
+      from(p in Product, where: p.id == ^product.id, select: [:views])
+      |> Repo.update_all(inc: [views: 1])
+
+    put_in(product.views, views)
+  end
 end
--- lib/hello_web/controllers/product_controller.ex
+++ lib/hello_web/controllers/product_controller.ex
@@ -27,7 +27,13 @@ defmodule HelloWeb.ProductController do
   end
 
   def show(conn, %{"id" => id}) do
-    product = Catalog.get_product!(id)
+    # product = Catalog.get_product!(id)
+    # Catalog.inc_page_views(product)
+    product =
+     id
+     |> Catalog.get_product!()
+     |> Catalog.inc_page_views()
+
     render(conn, :show, product: product)
   end
 
