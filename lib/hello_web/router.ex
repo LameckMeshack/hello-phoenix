f71edbb5b3d6328a013e1c2890e74b28f3e3e501
LameckMeshack <otienolameck327@gmail.com>
2024-04-01 10:16:40 +0300

    Add locale plug to router pipeline

--- lib/hello_web/router.ex
+++ lib/hello_web/router.ex
@@ -11,6 +11,7 @@ defmodule HelloWeb.Router do
     plug :protect_from_forgery
     plug :put_secure_browser_headers
     plug :fetch_current_user
+    plug HelloWeb.Plugs.Locale, "en"
   end
 
   pipeline :api do
