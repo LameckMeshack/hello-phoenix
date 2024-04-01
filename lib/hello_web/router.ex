ffed7cf657e3a010b090f0d1d75cd91bcef878f4
LameckMeshack <otienolameck327@gmail.com>
2024-04-02 00:12:15 +0300

    Add user and cart fetching plugs to router.ex

--- lib/hello_web/router.ex
+++ lib/hello_web/router.ex
@@ -11,7 +11,31 @@ defmodule HelloWeb.Router do
     plug :protect_from_forgery
     plug :put_secure_browser_headers
     plug :fetch_current_user
+    plug :fetch_current_cart
     plug HelloWeb.Plugs.Locale, "en"
+  end
+
+
+  defp fetch_current_user(conn, _) do
+    if user_uuid = get_session(conn, :current_uuid) do
+      assign(conn, :current_uuid, user_uuid)
+    else
+      new_uuid = Ecto.UUID.generate()
+      conn
+      |> assign(:current_uuid, new_uuid)
+      |> put_session(:current_uuid, new_uuid)
+    end
+  end
+
+  alias Hello.ShoppingCart
+
+  defp fetch_current_cart(conn, _opts) do
+    if cart = ShoppingCart.get_cart_by_user_uuid(conn.assigns.current_uuid) do
+      assign(conn, :cart, cart)
+    else
+      {:ok, new_cart} = ShoppingCart.create_cart(conn.assigns.current_uuid)
+      assign(conn, :cart, new_cart)
+    end
   end
 
   pipeline :api do
@@ -28,6 +52,12 @@ defmodule HelloWeb.Router do
     get "/hello/:messenger", HelloController, :show
 
     resources "/products", ProductController
+
+    resources "/cart_items", CartItemController, only: [:create, :delete]
+
+    get "/cart", CartController, :show
+
+    put "/cart", CartController, :update
   end
 
   # Other scopes may use custom stacks.
